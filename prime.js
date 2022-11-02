8b1f72338fb9d64fe4326689ac78797a9e15be4f
agarwalshantanu29 <agarwalshantanu29@gmail.com>
2022-11-02 19:40:02 +0530

    fixed

--- prime.js
+++ prime.js
@@ -1,12 +1,12 @@
 //check prime
 function prime(number){
-let flag=0;
+let count=0;
 for(let i=1;i<=number;i++){
 if(number%i==0){
-flag++;
+count++;
 }
 }
-if(flag==2)
+if(count==2)
 {
 return true;
 }
@@ -17,8 +17,8 @@ return false;
 }
 let check=prime(24)
 if(check==true){
-console.log("Number is Prime");
+console.log(Number+ " is Prime");
 }
 else{
-console.log("Number is not Prime");
+console.log(Number+" is not Prime");
 }
